5366246630755c0cbebdb34ee79c2f3555ec832d
einchieved <49752360+einchieved@users.noreply.github.com>
2021-10-20 14:00:12 +0200

    #9 fixed acceleration

--- Assets/IPortable.cs
+++ Assets/IPortable.cs
@@ -1,4 +1,8 @@
-﻿interface IPortable
+﻿using UnityEngine;
+
+interface IPortable
 {
     public bool IsPorting { get; set; }
+    public Vector3 PortingDirection { get; set; }
+    public Vector3 PortingFromDirection { get; set; }
 }
--- Assets/PortalTravel.cs
+++ Assets/PortalTravel.cs
@@ -10,22 +10,20 @@ public class PortalTravel : MonoBehaviour
         {
             return;
         }
+        // prepare object
+        IPortable portable = other.GetComponent<IPortable>();
+        portable.IsPorting = true;
+        portable.PortingDirection = OtherPortal.forward;
+        portable.PortingFromDirection = transform.forward;
 
         Rigidbody rb = other.GetComponent<Rigidbody>();
-        other.GetComponent<IPortable>().IsPorting = true;
-
-        // move
-        rb.position = /*new Vector3(0, 2.5f, 0); */OtherPortal.position + OtherPortal.forward * 2;
-        //other.transform.position = OtherPortal.position + OtherPortal.forward * 2;
-        // rotate
+        // position
+        rb.position = OtherPortal.position + OtherPortal.forward * 2;
+        // rotation
         float playerAngleDiff = Vector3.SignedAngle(transform.forward * -1, other.transform.forward, Vector3.up);
-        float x = /*other.transform.rotation*/rb.rotation.eulerAngles.x;
+        float x = rb.rotation.eulerAngles.x;
         float y = OtherPortal.rotation.eulerAngles.y;
-        float z = /*other.transform.rotation*/rb.rotation.eulerAngles.z;
-
+        float z = rb.rotation.eulerAngles.z;
         rb.rotation = Quaternion.Euler(x, y + playerAngleDiff, z);
-
-        //other.transform.rotation = Quaternion.Euler(x, y, z);
-        //other.transform.Rotate(Vector3.up * playerAngleDiff);
     }
 }
