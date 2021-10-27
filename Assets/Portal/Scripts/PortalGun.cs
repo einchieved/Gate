c8e8b54215f887f5ebc59a4a180feba7e6f1d13f
einchieved <49752360+einchieved@users.noreply.github.com>
2021-10-27 18:32:11 +0200

    #10: portals can be looked through

--- Assets/Portal/Scripts/PortalGun.cs
+++ Assets/Portal/Scripts/PortalGun.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class PortalGun : MonoBehaviour
 {
-    public Material blueMaterial, orangeMaterial;
+    public Material blueDefaultMaterial, orangeDefaultMaterial, blueMaterial, orangeMaterial;
     public GameObject portalPrefab;
 
     private GameObject bluePortal, orangePortal;
+    private MeshRenderer blueMeshRenderer, orangeMeshRenderer;
+    private RenderTexture blueRenderTexture, orangeRenderTexture;
     private Transform cam;
 
     private void Start()
     {
         cam = GetComponentInChildren<Camera>().transform;
+        blueRenderTexture = new RenderTexture(512, 1024, 24);
+        blueMaterial.SetTexture("Texture2D_e963585dc5b44a5b86d21edb99ea03f2", blueRenderTexture);
+        orangeRenderTexture = new RenderTexture(512, 1024, 24);
+        orangeMaterial.SetTexture("Texture2D_e963585dc5b44a5b86d21edb99ea03f2", orangeRenderTexture);
     }
 
     // Update is called once per frame
@@ -24,21 +30,23 @@ public class PortalGun : MonoBehaviour
                 Destroy(bluePortal);
             }
             bluePortal = newBluePortal;
-            bluePortal.GetComponent<MeshRenderer>().material = blueMaterial;
+            blueMeshRenderer = bluePortal.GetComponent<MeshRenderer>();
+            blueMeshRenderer.material = blueDefaultMaterial;
             UpdatePortals();
 
             return;
         }
 
         // place orange portal
-        if (Input.GetKeyDown(KeyCode.Mouse1) && CreatePortal(out GameObject neworangePortal))
+        if (Input.GetKeyDown(KeyCode.Mouse1) && CreatePortal(out GameObject newOrangePortal))
         {
             if (orangePortal != null)
             {
                 Destroy(orangePortal);
             }
-            orangePortal = neworangePortal;
-            orangePortal.GetComponent<MeshRenderer>().material = orangeMaterial;
+            orangePortal = newOrangePortal;
+            orangeMeshRenderer = orangePortal.GetComponent<MeshRenderer>();
+            orangeMeshRenderer.material = orangeDefaultMaterial;
             UpdatePortals();
         }
     }
@@ -55,12 +63,15 @@ public class PortalGun : MonoBehaviour
         }
 
         newPortal = Instantiate(portalPrefab, hitInfo.point, hitInfo.transform.rotation);
+        newPortal.transform.forward = hitInfo.transform.up;
         // adjust portal rotation to player rotation
         if (targetLayer == 9)
         {
             Vector3 eulerAngles = newPortal.transform.rotation.eulerAngles;
             newPortal.transform.rotation = Quaternion.Euler(eulerAngles.x, transform.rotation.eulerAngles.y + 180, eulerAngles.z);
         }
+
+        newPortal.GetComponentInChildren<Camera>().enabled = false;
         return true;
     }
 
@@ -68,8 +79,18 @@ public class PortalGun : MonoBehaviour
     {
         if (orangePortal != null && bluePortal != null)
         {
+            Camera tmpCam;
             bluePortal.GetComponent<PortalTravel>().OtherPortal = orangePortal.transform;
+            tmpCam = bluePortal.GetComponentInChildren<Camera>();
+            tmpCam.targetTexture = orangeRenderTexture;
+            tmpCam.enabled = true;
+            blueMeshRenderer.material = blueMaterial;
+
             orangePortal.GetComponent<PortalTravel>().OtherPortal = bluePortal.transform;
+            tmpCam = orangePortal.GetComponentInChildren<Camera>();
+            tmpCam.targetTexture = blueRenderTexture;
+            tmpCam.enabled = true;
+            orangeMeshRenderer.material = orangeMaterial;
         }
     }
 }
