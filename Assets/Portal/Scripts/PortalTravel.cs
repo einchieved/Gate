c6286f78c7af66619f1271979a3cb88e644834be
einchieved <49752360+einchieved@users.noreply.github.com>
2021-11-01 12:08:00 +0100

    #10: fixed keeping velocity

--- Assets/Portal/Scripts/PortalTravel.cs
+++ Assets/Portal/Scripts/PortalTravel.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PortalTravel : MonoBehaviour
 {
+    public Transform spawnPosition;
+
     public Transform OtherPortal { get; set; }
 
     private void OnTriggerEnter(Collider other)
@@ -13,12 +15,13 @@ public class PortalTravel : MonoBehaviour
         // prepare object
         IPortable portable = other.GetComponent<IPortable>();
         portable.IsPorting = true;
-        portable.PortingDirection = OtherPortal.forward;
-        portable.PortingFromDirection = transform.forward;
+        portable.PortingDirection = OtherPortal.up;
+        portable.PortingFromDirection = transform.up;
 
         Rigidbody rb = other.GetComponent<Rigidbody>();
         // position
-        rb.position = OtherPortal.position + OtherPortal.forward * 2;
+        //rb.position = OtherPortal.position + OtherPortal.forward * 2;
+        rb.position = OtherPortal.GetComponent<PortalTravel>().spawnPosition.position;
         // rotation
         float playerAngleDiff = Vector3.SignedAngle(transform.forward * -1, other.transform.forward, Vector3.up);
         float x = rb.rotation.eulerAngles.x;
