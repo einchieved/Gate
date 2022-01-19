94312faae7d42414a730c4cc86fa758fd8faf331
einchieved <49752360+einchieved@users.noreply.github.com>
2022-01-19 17:52:07 +0100

    #45 fixed loading

--- Assets/MultilevelGame/Scripts/DestinationScript.cs
+++ Assets/MultilevelGame/Scripts/DestinationScript.cs
@@ -1,25 +1,32 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
 
-public class DestinationScript : MonoBehaviour
+public class DestinationScript : MonoBehaviourPun
 {
     public int nextLevel;
     private void OnCollisionEnter(Collision other)
     {
-        Debug.Log("Trigger entered");
+        Debug.LogError("Trigger entered");
         if (other.gameObject.CompareTag("CompanionCube"))
         {
             if (PhotonNetwork.IsMasterClient)
             {
-                GameObject playerOne = GameObject.FindWithTag("P1");
-                playerOne.transform.parent = null;
-
+                photonView.RPC(nameof(LoadPrep), RpcTarget.All);
                 PhotonNetwork.Destroy(other.gameObject);
                 PhotonNetwork.LoadLevel("Level0" + nextLevel); 
             }
         }    
+    }
+
+    [PunRPC]
+    public void LoadPrep()
+    {
+        GameObject playerOne = GameObject.FindWithTag("P1");
+        playerOne.transform.parent = null;
+        DontDestroyOnLoad(playerOne);
+
+        GameObject playerTwo = GameObject.FindWithTag("P2");
+        playerTwo.transform.parent = null;
+        DontDestroyOnLoad(playerTwo);
     }
 }
